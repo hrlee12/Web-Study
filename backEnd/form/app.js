a2a2cfa9d2d115e5f85c07177292736e77c42561
hrlee12 <gyfl5542@naver.com>
2023-03-28 18:56:52 +0900

    study: 폼 학습을 위한 기본 프로젝트 구조 생성

---
+++ backEnd/form/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+// view engine 등록
+app.set("view engine", "ejs");
+// ejs를 담을 views 폴더 경로 설정
+app.use("/views", express.static(__dirname + "/views"));
+
+// 라우팅(routing) : 경로 설정
+// 브라우저에서 어떤 url로 접속했을 때 어떤 페이지를 보여줄 것인가!!
+
+// localhost:PORT/ 접속했을 때, index.ejs를 보여주겠다
+app.get("/", function (req, res) {
+  // views/index.ejs 파일을 찾아서 응답
+  res.render("index");
+});
+
+app.listen(PORT, function () {
+  console.log("웹 서버 실행!!");
+  console.log(`http://localhost:${PORT}`);
+});
