1b93ffd46045e0cdc5bfbd3ef047fa3e9d8b27c3
hrlee12 <gyfl5542@naver.com>
2023-03-28 19:57:20 +0900

    get, post 방식 비교

--- backEnd/form/app.js
+++ backEnd/form/app.js
@@ -7,13 +7,32 @@ app.set("view engine", "ejs");
 // ejs를 담을 views 폴더 경로 설정
 app.use("/views", express.static(__dirname + "/views"));
 
+// body-parser 사용하기
+// post 요청으로 들어오는 모든 형식의 데이터를 파싱
+app.use(express.urlencoded({ extended: true }));
+// json 형태로 데이터를 주고 받음.
+app.use(express.json());
+
 // 라우팅(routing) : 경로 설정
 // 브라우저에서 어떤 url로 접속했을 때 어떤 페이지를 보여줄 것인가!!
 
 // localhost:PORT/ 접속했을 때, index.ejs를 보여주겠다
 app.get("/", function (req, res) {
   // views/index.ejs 파일을 찾아서 응답
-  res.render("index");
+  const myTitle = "폼 실습을 합시다~~~";
+  res.render("index", { title: myTitle });
+});
+
+app.get("/getForm", function (req, res) {
+  // form으로 submit 하면 request 객체가 서버로 감.
+  // request 객체의 query는 입력한 값
+  console.log(req.query);
+  res.send("get 요청 성공!!!");
+});
+
+app.post("/postForm", function (req, res) {
+  console.log(req.body);
+  res.send("post 요청 성공!!");
 });
 
 app.listen(PORT, function () {
