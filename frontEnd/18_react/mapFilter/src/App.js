0b48d9bc8e833fab252ab673167f26e16234f7a8
hrlee12 <gyfl5542@naver.com>
2023-04-22 11:38:04 +0900

    study: react_map, filter practice

---
+++ frontEnd/18_react/mapFilter/src/App.js
@@ -0,0 +1,15 @@
+import Alphabet from "./Alphabet";
+import RenderInf from "./RenderInf";
+import RenderInfTable from "./RenderInfTable";
+
+function App() {
+  return (
+    <div className="App">
+      <RenderInfTable />
+      {/* <RenderInf /> */}
+      {/* <Alphabet /> */}
+    </div>
+  );
+}
+
+export default App;
